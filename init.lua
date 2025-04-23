0c680b79dd16b15fc0a98e598f236201120b411c
Connor Dear <connordear@gmail.com>
2025-04-22 18:30:16 -0600

    added colemak rebindings, theming, initial changes

--- init.lua
+++ init.lua
@@ -1,89 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -102,7 +16,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -180,10 +94,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -200,6 +114,66 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+_G.colemak_keymaps_enabled = false
+
+local function set_colemak_keymaps()
+  if not _G.colemak_keymaps_enabled then
+    return
+  end
+  local opts = { silent = true }
+
+  vim.keymap.set({ 'n', 'v' }, 'h', 'm', opts)
+  vim.keymap.set({ 'n', 'v' }, 'j', 'n', opts)
+  vim.keymap.set({ 'n', 'v' }, 'h', 'e', opts)
+  vim.keymap.set({ 'n', 'v' }, 'l', 'i', opts)
+  vim.keymap.set({ 'n', 'v' }, 'm', 'h', opts)
+  vim.keymap.set({ 'n', 'v' }, 'n', 'j', opts)
+  vim.keymap.set({ 'n', 'v' }, 'e', 'k', opts)
+  vim.keymap.set({ 'n', 'v' }, 'i', 'l', opts)
+  vim.keymap.set({ 'n' }, 's', '')
+  _G.colemak_keymaps_enabled = true
+  vim.notify('Colemak keymaps ENABLED', vim.log.levels.INFO, { fg = '#8888FF' }) -- Blue-ish notify
+end
+
+local function clear_colemak_keymaps()
+  pcall(vim.keymap.del, 'n', 'h')
+  pcall(vim.keymap.del, 'n', 'j')
+  pcall(vim.keymap.del, 'n', 'k')
+  pcall(vim.keymap.del, 'n', 'l')
+  pcall(vim.keymap.del, 'n', 'm')
+  pcall(vim.keymap.del, 'n', 'n')
+  pcall(vim.keymap.del, 'n', 'e')
+  pcall(vim.keymap.del, 'n', 'i')
+  pcall(vim.keymap.del, 'v', 'h')
+  pcall(vim.keymap.del, 'v', 'j')
+  pcall(vim.keymap.del, 'v', 'k')
+  pcall(vim.keymap.del, 'v', 'l')
+  pcall(vim.keymap.del, 'v', 'm')
+  pcall(vim.keymap.del, 'v', 'n')
+  pcall(vim.keymap.del, 'v', 'e')
+  pcall(vim.keymap.del, 'v', 'i')
+  pcall(vim.keymap.del, 'n', 's')
+  _G.colemak_keymaps_enabled = false
+  vim.notify('Colemak keymaps DISABLED', vim.log.levels.WARN)
+end
+
+vim.api.nvim_create_user_command('ToggleColemakKeymaps', function()
+  if _G.colemak_keymaps_enabled then
+    clear_colemak_keymaps()
+  else
+    _G.colemak_keymaps_enabled = true
+    set_colemak_keymaps()
+  end
+end, {
+  desc = 'Enable/Disable Colemak-DH specific keybindings',
+})
+
+vim.keymap.set('n', '-', function()
+  local buf_name = vim.api.nvim_buf_get_name(0)
+  local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
+  MiniFiles.open(path)
+  MiniFiles.reveal_cwd()
+end, { desc = 'Open Mini Files' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -873,21 +847,31 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
+      require('kanagawa').setup {
+        commentStyle = {
+          italic = false,
         },
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd 'colorscheme kanagawa'
     end,
+    -- 'folke/tokyonight.nvim',
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- config = function()
+    --   ---@diagnostic disable-next-line: missing-fields
+    --   require('tokyonight').setup {
+    --     styles = {
+    --       comments = { italic = false }, -- Disable italics in comments
+    --     },
+    --   }
+
+    --   -- Load the colorscheme here.
+    --   -- Like many other themes, this one has different styles, and you could load
+    --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    --   vim.cmd.colorscheme 'tokyonight-moon'
+    -- end,
   },
 
   -- Highlight todo, notes, etc in comments
@@ -910,6 +894,8 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+
+      require('mini.files').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
@@ -965,12 +951,12 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
@@ -1003,6 +989,9 @@ require('lazy').setup({
     },
   },
 })
+
+require 'autocmds'
+set_colemak_keymaps()
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/autocmds.lua
@@ -0,0 +1,54 @@
+-- Ensure the global flag is defined (important!)
+if _G.colemak_keymaps_enabled == nil then
+  _G.colemak_keymaps_enabled = false
+  print 'WARN: _G.colemak_keymaps_enabled was not defined, setting to false.'
+end
+
+local miniFilesColemakGroup = vim.api.nvim_create_augroup('MiniFilesConditionalMaps', { clear = true })
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = miniFilesColemakGroup,
+  pattern = 'minifiles',
+  desc = 'Setup conditional Colemak keymaps for mini.files respecting user config',
+  callback = function(args)
+    -- Ensure mini.files module is loaded to access its functions safely
+    local mf = require 'mini.files'
+    if not mf then
+      vim.notify('mini.files module not found for autocmd', vim.log.levels.ERROR)
+      return
+    end
+
+    local buf = args.buf
+    -- Buffer-local options are key!
+    local opts = { buffer = buf, noremap = true, silent = true }
+
+    -- Define functions for the standard mini.files actions
+    local function mf_go_in()
+      mf.go_in()
+    end
+    local function mf_go_out()
+      mf.go_out()
+    end
+
+    -- IMPORTANT: Your setup() mappings for 'I', 'M' are still active.
+    -- This autocmd sets buffer-local maps for h,j,k,l, i,u, m
+    -- which take precedence over global/setup maps for *these specific keys*
+    -- in this buffer.
+
+    -- === Conditional Mappings based on _G.colemak_keymaps_enabled ===
+
+    -- --- Navigation and Mark Set (h, j, k, l) ---
+
+    -- Physical 'h' key
+    vim.keymap.set('n', 'i', function()
+      if _G.colemak_keymaps_enabled then
+        mf_go_in()
+      end
+    end, opts)
+
+    -- Physical 'j' key (Assuming j is still Down)
+    vim.keymap.set('n', 'm', function()
+      mf_go_out()
+    end, opts)
+  end, -- end callback
+}) -- end autocmd
